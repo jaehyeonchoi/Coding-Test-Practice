90cd903e6dbd05980ad8d93b798c02697a319107
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2025-02-07 15:27:41 +0900

    Create 2439.java

---
+++ 0x02/2439.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(" ".repeat(Math.max(0, N - i)));
+            sb.append("*".repeat(Math.max(0, i)));
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
