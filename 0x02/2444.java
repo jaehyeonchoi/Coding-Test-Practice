2da27e60920736f3d9da3ab88388161a3c775055
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2025-02-07 15:53:46 +0900

    Create 2444.java

---
+++ 0x02/2444.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(" ".repeat(Math.max(0, N - i)));
+            sb.append("*".repeat(Math.max(0, 2 * i - 1)));
+            sb.append("\n");
+        }
+
+        for (int i = 1; i <= N - 1; i++) {
+            sb.append(" ".repeat(Math.max(0, i)));
+            sb.append("*".repeat(Math.max(0, 2 * (N - i) - 1)));
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
