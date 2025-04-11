6ba8e75ad15c0e7b0c5b8ea2ce982a32bddddd57
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2025-04-11 23:30:44 +0900

    Create 15657.java

---
+++ 0x0C/15657.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static int[] input;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        input = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(input);
+
+        sb = new StringBuilder();
+        recursive(0, 0);
+        System.out.println(sb);
+    }
+
+    public static void recursive(int k, int last) {
+        if (k == M) { // M개의 숫자를 모두 선택한 경우
+            for (int i : arr) {
+                sb.append(input[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = last; i < N; i++) {
+            arr[k] = i;
+            recursive(k + 1, i);
+        }
+    }
+}
