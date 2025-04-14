20c504b247481432387bfb7288527597364a967c
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2025-04-14 18:37:28 +0900

    Create 15664.java

---
+++ 0x0C/15664.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static int[] input;
+    static StringBuilder sb;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        input = new int[N];
+        arr = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(input);
+
+        sb = new StringBuilder();
+        recursive(0, -1);
+
+        System.out.println(sb);
+    }
+
+    public static void recursive(int k, int last) {
+        if (k == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int temp = -1; // 해당 재귀에서 마지막에 사용해본 숫자
+        for (int i = last + 1; i < N; i++) {
+            if (temp != input[i]) { // 마지막에 사용해본 숫자를 또 사용하면 중복 수열이 되므로 pass
+                arr[k] = input[i];
+                temp = input[i];
+                recursive(k + 1, i);
+            }
+        }
+    }
+}
