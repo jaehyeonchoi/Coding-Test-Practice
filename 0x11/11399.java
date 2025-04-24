6177cdd76cc61826da23025c671b5199384b3775
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2025-04-24 21:00:54 +0900

    Create 11399.java

---
+++ 0x11/11399.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static Integer[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new Integer[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += arr[i] * (N - i);
+        }
+        System.out.println(sum);
+    }
+}
