cdd13d579f87981c888d9f59777f7e9e558d03c8
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2025-04-24 18:36:33 +0900

    Create 11047.java

---
+++ 0x11/11047.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, K;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            if (K >= arr[i]) {
+                count += K / arr[i];
+                K %= arr[i];
+            }
+        }
+        System.out.println(count);
+    }
+}
