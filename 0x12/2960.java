45fc88bd7e5fcedea0c53ffb140959b8c8a32ea0
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2025-05-08 18:10:34 +0900

    Create 2960.java

---
+++ 0x12/2960.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, K;
+    static boolean[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        arr = new boolean[N + 1];
+
+        int count = 0;
+        while (true) {
+            int x = -1;
+            for (int i = 2; i <= N; i++) {
+                if (!arr[i]) {
+                    x = i; // 아직 지우지 않은 수 중에서 가장 작은 수 찾기
+                    break;
+                }
+            }
+            int curr = x; // 현재 지우려고 하는 수 (x의 배수)
+            while (curr <= N) {
+                if (!arr[curr]) { // 아직 안 지워진 경우
+                    arr[curr] = true; // 지우기
+                    if (++count == K) { // K번째 지워진 수인 경우
+                        System.out.println(curr); // 출력 후 종료
+                        return;
+                    }
+                }
+                curr += x;
+            }
+        }
+    }
+}
