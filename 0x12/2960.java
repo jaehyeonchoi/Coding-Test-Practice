06983bfa947a22c1fd17689fceba448af9f44b48
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2025-05-08 19:19:02 +0900

    Update 2960.java

--- 0x12/2960.java
+++ 0x12/2960.java
@@ -13,24 +13,19 @@ public class Main {
         arr = new boolean[N + 1];
 
         int count = 0;
-        while (true) {
-            int x = -1;
-            for (int i = 2; i <= N; i++) {
-                if (!arr[i]) {
-                    x = i; // 아직 지우지 않은 수 중에서 가장 작은 수 찾기
-                    break;
-                }
-            }
-            int curr = x; // 현재 지우려고 하는 수 (x의 배수)
-            while (curr <= N) {
-                if (!arr[curr]) { // 아직 안 지워진 경우
-                    arr[curr] = true; // 지우기
-                    if (++count == K) { // K번째 지워진 수인 경우
-                        System.out.println(curr); // 출력 후 종료
-                        return;
+        for (int i = 2; i <= N; i++) {
+            if (!arr[i]) { // 아직 지우지 않은 수 중에서 가장 작은 수 찾기
+                int curr = i; // 현재 지우려고 하는 수 (x의 배수)
+                while (curr <= N) {
+                    if (!arr[curr]) { // 아직 안 지워진 경우
+                        arr[curr] = true; // 지우기
+                        if (++count == K) { // K번째 지워진 수인 경우
+                            System.out.println(curr); // 출력 후 종료
+                            return;
+                        }
                     }
+                    curr += i;
                 }
-                curr += x;
             }
         }
     }
