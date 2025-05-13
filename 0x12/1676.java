3bd5d7a4755290b1281b3414f015070ee52e085c
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2025-05-14 00:54:48 +0900

    Update 1676.java

--- 0x12/1676.java
+++ 0x12/1676.java
@@ -6,20 +6,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        int sum2 = 0; // 팩토리얼에 들어있는 2의 거듭제곱 지수
-        int sum5 = 0; // 팩토리얼에 들어있는 5의 거듭제곱 지수
-        for (int i = 2; i <= N; i++) { // 2 ~ N까지 각각 2, 5가 몇 개씩 곱해져 있는지 계산
-            int exp2 = 1;
-            int exp5 = 1;
-            while (i % Math.pow(2, exp2) == 0) {
-                exp2++;
-            }
-            while (i % Math.pow(5, exp5) == 0) {
-                exp5++;
-            }
-            sum2 += exp2 - 1;
-            sum5 += exp5 - 1;
+        
+        int count5 = 0; // 무조건 5보다 2가 많으므로 5의 개수만 세도 됨
+        while (N >= 5) {
+            count5 += N / 5;
+            N /= 5;
         }
-        System.out.println(Math.min(sum2, sum5)); // 2의 지수, 5의 지수 중 최솟값이 10의 지수가 됨
+        System.out.println(count5);
     }
 }
