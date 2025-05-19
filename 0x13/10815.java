12c19319bac1216e7c83fc969b052c2a210ccbe0
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2025-05-19 21:56:00 +0900

    Create 10815.java

---
+++ 0x13/10815.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            System.out.println(func(x));
+        }
+    }
+
+    private static int func(int x) {
+        int start = 0;
+        int end = N - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] > x) {
+                end = mid - 1;
+            }
+            else if (arr[mid] < x) {
+                start = mid + 1;
+            }
+            else if (arr[mid] == x) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
