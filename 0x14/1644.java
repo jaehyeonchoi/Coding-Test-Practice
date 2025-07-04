1ef097201c0b8cc4f9ab127842216265ce054b29
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2025-07-04 16:44:54 +0900

    Update 1644.java

--- 0x14/1644.java
+++ 0x14/1644.java
@@ -29,25 +29,23 @@ public class Main {
             }
         }
 
-        int count = 0; // 가능한 연속된 소수합의 개수
         int start = 0; // 연속된 소수의 시작 인덱스
         int end = 0; // 연속된 소수의 끝 인덱스
+        int sum = 0; // 연속된 소수의 합
+        int count = 0; // 가능한 연속된 소수합의 개수
 
-        while (end < prime.size() && start <= end) {
-            int sum = 0; // 연속된 소수의 합
-            for (int i = start; i <= end; i++) {
-                sum += prime.get(i);
-            }
-
-            if (sum < N) { // 합이 N보다 작은 경우
-                end++; // 끝 인덱스를 늘리기 -> 합을 늘리기
-            }
+        while (start <= end) {
             if (sum > N) { // 합이 N보다 큰 경우
-                start++; // 시작 인덱스를 늘리기 -> 합을 줄이기
+                sum -= prime.get(start++); // 가장 앞에 있던 숫자를 빼기
             }
-            if (sum == N) { // 합이 N과 같은 경우
-                count++; // 소수합의 개수
-                end++; // 끝 인덱스를 늘리기 -> 새로운 조합 찾기
+            else { // 합이 N과 같거나 작은 경우
+                if (sum == N) { // 합이 N과 같으면
+                    count++; // 소수합의 개수 + 1
+                }
+                if (end == prime.size()) { // end를 늘려야하는데 이미 끝에 도달한 경우
+                    break; // 탐색종료
+                }
+                sum += prime.get(end++); // 가장 뒤에 새로운 숫자를 더하기
             }
         }
         System.out.println(count);
