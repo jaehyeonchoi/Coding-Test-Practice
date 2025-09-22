f3cc25875a9abd18bb5357d027883be192a0ea06
jaehyeonchoi <132991315+jaehyeonchoi@users.noreply.github.com>
2025-09-23 01:22:27 +0900

    Update 15649.java

--- 0x0C/15649.java
+++ 0x0C/15649.java
@@ -2,11 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[] arr;
-    static boolean[] used;
+    private static int N, M;
+    private static int[] arr;
+
+    private static boolean[] used;
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -16,24 +18,27 @@ public class Main {
         arr = new int[M];
         used = new boolean[N];
 
-        recursive(0);
+        func(0);
     }
 
-    public static void recursive(int k) {
-        if (k == M) {
-            for (int i : arr) {
-                System.out.print(i + " ");
+    public static void func(int k) {
+
+        // 길이 M 짜리 수열을 완성하고 나면 출력 후 리턴 -> 재귀 종료
+        if (k == M) { 
+            StringBuilder sb = new StringBuilder(); // print로 각각 출력하는 것보다 StringBuilder로 String을 먼저 만든 후 출력하는 것이 더 빠름
+            for (int x : arr) {
+                sb.append(x + 1).append(" ");
             }
-            System.out.println();
+            System.out.println(sb);
             return;
         }
 
         for (int i = 0; i < N; i++) {
             if (!used[i]) {
-                arr[k] = i + 1;
-                used[i] = true;
-                recursive(k + 1);
-                used[i] = false;
+                arr[k] = i;
+                used[i] = true; // 재귀적으로 호출된 곳에서 다시 사용하지 않도록 used를 true로 표시
+                func(k + 1);
+                used[i] = false; // 메서드 결과가 리턴된 후에는 다시 사용할 수 있도록 used 값을 false로 돌려놓기
             }
         }
     }
